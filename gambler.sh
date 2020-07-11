ae70bf76d0917af3ef28dbb69407cfa7d46867ce
aadityaawasthi9767 <aadityaawasthi9767@gmail.com>
2020-07-11 10:22:27 +0530

    [Aaditya] UC2 UPDATED: Wins and Losses

--- gambler.sh
+++ gambler.sh
@@ -3,3 +3,24 @@
 #CONSTANTS
 DAILY_AMOUNT=100;
 DAILY_BET=1;
+WON=1;
+LOSS=0;
+
+duplicateAmount=DAILY_AMOUNT;
+
+
+betCheck=$((RANDOM%2));
+   case $betCheck in
+               $WON)
+                     duplicateAmount=$((duplicateAmount+1));
+                     echo "WON this bet";;
+
+               $LOSS)
+                     duplicateAmount=$((duplicateAmount-1));
+                     echo "LOST this bet";;
+
+                  *)
+                     echo "Wrong Input";
+   esac
+
+echo "New Amount: " $duplicateAmount;
