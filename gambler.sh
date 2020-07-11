8343b5a82213ce3a62e58684d2905afbb3feb335
aadityaawasthi9767 <aadityaawasthi9767@gmail.com>
2020-07-11 10:29:39 +0530

    [Aaditya] UC3 UPDATED: upper and lower conditions

--- gambler.sh
+++ gambler.sh
@@ -5,9 +5,13 @@ DAILY_AMOUNT=100;
 DAILY_BET=1;
 WON=1;
 LOSS=0;
+MAX_LIMIT=150;
+MIN_LIMIT=50;
 
-duplicateAmount=DAILY_AMOUNT;
+duplicateAmount=$((DAILY_AMOUNT));
 
+while [[ $duplicateAmount -ge $MIN_LIMIT && $duplicateAmount -le $MAX_LIMIT ]]
+do
 
 betCheck=$((RANDOM%2));
    case $betCheck in
@@ -22,5 +26,5 @@ betCheck=$((RANDOM%2));
                   *)
                      echo "Wrong Input";
    esac
-
+done
 echo "New Amount: " $duplicateAmount;
